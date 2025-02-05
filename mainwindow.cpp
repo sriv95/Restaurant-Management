7ac83bf07b2d80c3b270b7a5b7434bfa96f3c9ce
Fuuun1024 <worapopfong@gmail.com>
2025-02-06 00:09:09 +0700

    commit added and Edited func_Table

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,7 +17,7 @@ RestuarantManagement::RestuarantManagement(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
-
+    updateTablesStatus();
     for(int i=1;i<=Table_Count;++i){
         QString btnName = QString("Table_").append(QString::number(i));
         QPushButton *button = this->findChild<QPushButton *>(btnName);
@@ -32,7 +32,7 @@ RestuarantManagement::RestuarantManagement(QWidget *parent)
 RestuarantManagement::~RestuarantManagement()
 {}
 
-int RestuarantManagement::GetSelectingTableNo(){ // edited by fong
+int RestuarantManagement::GetSelectingTableNo(){ // used by fong
     return ui.SelectingTable->text().toInt();
 }
 
@@ -53,20 +53,37 @@ void RestuarantManagement::on_TableBtn_clicked()
     QString buttonName = buttonSender->objectName(); // Get the name from the button
     QString table_no = buttonName.split("_").last();
     SetSelectingTable(table_no);
+    Changeseats(); // added by fong
 
     // buttonSender->setText("Table#"+table_no+"\n .."); //⚠️needs getTableStatus
 }
 
-// void RestuarantManagement::updateTablesStatus(string TablesStatus[] ) // added by fong
-// {
+void RestuarantManagement::updateTablesStatus() // added by fong
+{
+    json Tables;
+    getData(Tables,"Tables");
 
-// }
+
+    for(int i=1;i<=Table_Count;++i){
+        int seat = Tables [i-1]["Seats"];
+        QString Reserved= QString::fromStdString(Tables[i-1]["Reserved"]);
+
+        QString btnName = QString("Table_").append(QString::number(i));
+        QPushButton *button = this->findChild<QPushButton *>(btnName);
+        if(button){
+            if(seat != 0)button->setText("Table#"+ QString::number(i) + "\n" + QString::number(seat)+ "👤");
+            else if(Reserved !="")button->setText("Table#"+ QString::number(i) + "\nReserved: "+ Reserved);
+            else button->setText("Table#"+ QString::number(i) + "\nAvallable");
+
+        }
+        else  qDebug()<<"Error: Button Not Found (Button Name: "<<btnName<<")";
+    }
+}
 
 
 void RestuarantManagement::on_RefreshBtn_clicked()  //Edited by fong
 {
-    //json Tables;
-    //getData(Tables,"Tables");
+    updateTablesStatus();
 }
 
 void RestuarantManagement::on_CheckBills_clicked()
@@ -83,12 +100,31 @@ void RestuarantManagement::on_OpenTableBtn_clicked()
     OpenTableDialog.exec();
 }
 
-void RestuarantManagement::onTableReturnValue(const QString &data){ //fong
-    QString x = data; //handle unused variable
-    qDebug("x");
+void RestuarantManagement::onTableReturnValue(const QString &data){ //edited by fong
+    json Tables;
+    getData(Tables,"Tables");
+    int seat = data.toInt(); //handle unused variable
+    int No = GetSelectingTableNo() - 1 ;
+    qDebug() << "Seats: " << seat << "TableNo: " <<GetSelectingTableNo();
+    Tables [No]["Seats"] = seat;
+
+    setData(Tables, "Tables");
+    updateTablesStatus();
 
 }
 
+void RestuarantManagement::Changeseats()
+{
+    json Tables;
+    getData(Tables,"Tables");
+
+    int No = GetSelectingTableNo() - 1 ;
+    if(No >= 0)
+    {
+    if(Tables[No]["Seats"] != 0) ui.OpenTableBtn->setText("Change seat");
+    else ui.OpenTableBtn->setText("Open Table");
+    }
+}
 
 void RestuarantManagement::on_ReserveBtn_clicked()
 {
