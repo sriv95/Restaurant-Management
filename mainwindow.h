9ada3fd1ee33ddd768e8992f2e6d14e81f06461c
GGTTGTGt <zz814170@gmail.com>
2025-02-06 23:56:51 +0700

    coded

--- mainwindow.h
+++ mainwindow.h
@@ -42,4 +42,32 @@ private:
     void loadData();
     Ui::RestuarantManagementClass ui;
     OpenTableDialog *openTableDialog; // Declare OpenTableDialog as a member
+    void getBills(const json& Bills, const json& Menus, vector<string>& BillsNames, vector<int>& BillsPrices) {
+        if (Bills.is_array() && Bills.size() >= 3) {
+            const auto& names = Bills[0];
+            const auto& quantities = Bills[1];
+            const auto& extras = Bills[2];
+
+            if (names.is_array() && quantities.is_array() && extras.is_array()) {
+                for (size_t i = 0; i < names.size(); ++i) {
+                    string menuName = names[i].get<string>();
+                    int quantity = quantities[i].get<int>();
+                    int extraCharge = extras[i].get<int>();
+
+                    int menuPrice = 0;
+                    for (const auto& menu : Menus) {
+                        if (menu[0] == menuName) {
+                            menuPrice = menu[1].get<int>();
+                            break;
+                        }
+                    }
+
+                    if (menuPrice > 0) {
+                        BillsNames.push_back(menuName);
+                        BillsPrices.push_back((menuPrice * quantity) + extraCharge);
+                    }
+                }
+            }
+        }
+    }
 };
