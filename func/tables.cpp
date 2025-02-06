aacdefc4b4bdd0c517597aafe902365cb78846c6
GGTTGTGt <zz814170@gmail.com>
2025-02-06 21:00:06 +0700

    coded

--- func/tables.cpp
+++ func/tables.cpp
@@ -1,34 +1,30 @@
+#include "header/tables.h"
 
-// ฟังก์ชันรับค่า Bills และตั้งค่าให้กับ BillsNames & BillsPrices
 void getBills(const json& Bills, const json& Menus, vector<string>& BillsNames, vector<int>& BillsPrices) {
-    
-    if (Bills.is_array() && Bills.size() >= 2) { //ตรวจสอบว่า Bills เป็น array และมีขนาดไม่น้อยกว่า 2
-
+    if (Bills.is_array() && Bills.size() >= 3) {
         const auto& names = Bills[0];
         const auto& quantities = Bills[1];
-        // บิลชื่อเมนูอยู่ที่ index 0 และจำนวนเมนูอยู่ที่ index 1
+        const auto& extras = Bills[2];
 
-        if (names.is_array() && quantities.is_array()) { //ตรวจสอบว่า names และ quantities เป็น array
-            for (size_t i = 0; i < names.size(); ++i) { //ลูปผ่านทุกชื่อเมนูและหาข้อมูลราคาและจำนวนของเมนูนั้น
-                string menuName = names[i].get<string>(); //ชื่อเมนูจาก array names
-                int quantity = quantities[i].get<int>(); //จำนวนของเมนูที่สั่งจาก array quantities
+        if (names.is_array() && quantities.is_array() && extras.is_array()) {
+            for (size_t i = 0; i < names.size(); ++i) {
+                string menuName = names[i].get<string>();
+                int quantity = quantities[i].get<int>();
+                int extraCharge = extras[i].get<int>();
 
-                // ค้นหาราคาของเมนูจาก Menus
                 int menuPrice = 0;
-                for (const auto& menu : Menus) { //ลูปผ่าน Menus เพื่อหาข้อมูลราคาเมนูที่ตรงกับ menuName
-                    if (menu[0] == menuName) { //ตรวจสอบว่าเมนูใน Menus ตรงกับชื่อเมนู (menuName)
+                for (const auto& menu : Menus) {
+                    if (menu[0] == menuName) {
                         menuPrice = menu[1].get<int>();
                         break;
                     }
                 }
 
-                // เพิ่มชื่อเมนูและราคาลงใน BillsNames & BillsPrices
                 if (menuPrice > 0) {
                     BillsNames.push_back(menuName);
-                    BillsPrices.push_back(menuPrice * quantity); // ราคาคูณจำนวนที่สั่ง
+                    BillsPrices.push_back((menuPrice * quantity) + extraCharge);
                 }
             }
         }
     }
 }
-
