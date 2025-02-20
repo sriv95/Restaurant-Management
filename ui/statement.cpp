13d2ca43c1ff3f3614e69fc9ea8f82c7bb0a2978
sriv-PC <srivichai95@gmail.com>
2025-02-20 10:10:16 +0700

    Add Income and Expense tabs

--- ui/statement.cpp
+++ ui/statement.cpp
@@ -12,9 +12,20 @@ Statement::Statement(QWidget *parent)
 {
     ui->setupUi(this);
 
+    //All Tab Setup
     ui->Statement_Table->verticalHeader()->setVisible(false);
     ui->Statement_Table->horizontalHeader()->setSortIndicatorShown(false);
     ui->Statement_Table->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeMode::Stretch);
+
+    //Income Tab Setup
+    ui->Income_Table->verticalHeader()->setVisible(false);
+    ui->Income_Table->horizontalHeader()->setSortIndicatorShown(false);
+    ui->Income_Table->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeMode::Stretch);
+
+    //Expense Tab Setup
+    ui->Expense_Table->verticalHeader()->setVisible(false);
+    ui->Expense_Table->horizontalHeader()->setSortIndicatorShown(false);
+    ui->Expense_Table->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeMode::Stretch);
 
     //Add Year Input
     QComboBox *Year_Input = ui->Year_Input;
@@ -64,6 +75,8 @@ void Statement::loadData()
 
     json statementData = data["Statement"];
     ui->Statement_Table->setRowCount(0);
+    ui->Income_Table->setRowCount(0);
+    ui->Expense_Table->setRowCount(0);
     double totalIncome = 0, totalExpense = 0;
     int row = 0;
 
@@ -89,13 +102,32 @@ void Statement::loadData()
         ui->Statement_Table->setItem(row, 2, new QTableWidgetItem(name));
         ui->Statement_Table->setItem(row, 3, new QTableWidgetItem(QString::number(quantity)));
         QTableWidgetItem *priceItem = new QTableWidgetItem(QString::number(price, 'f', 2) + "฿");
+        QTableWidgetItem *priceItem2 = new QTableWidgetItem(QString::number(price, 'f', 2) + "฿");
 
-        if (price > 0) {
+        if (price > 0) { //Income
             priceItem->setBackground(QColor(60, 179, 113)); // Green for income
+            priceItem2->setBackground(QColor(60, 179, 113));
             totalIncome += price;
-        } else {
+
+            int rowCount = ui->Income_Table->rowCount();
+            ui->Income_Table->insertRow(rowCount);
+            ui->Income_Table->setItem(rowCount, 0, new QTableWidgetItem(date));
+            ui->Income_Table->setItem(rowCount, 1, new QTableWidgetItem(time));
+            ui->Income_Table->setItem(rowCount, 2, new QTableWidgetItem(name));
+            ui->Income_Table->setItem(rowCount, 3, new QTableWidgetItem(QString::number(quantity)));
+            ui->Income_Table->setItem(rowCount, 4, priceItem2);
+        } else { //Expense
             priceItem->setBackground(QColor(255, 99, 71)); // Red for expense
+            priceItem2->setBackground(QColor(255, 99, 71));
             totalExpense += -price;
+
+            int rowCount = ui->Expense_Table->rowCount();
+            ui->Expense_Table->insertRow(rowCount);
+            ui->Expense_Table->setItem(rowCount, 0, new QTableWidgetItem(date));
+            ui->Expense_Table->setItem(rowCount, 1, new QTableWidgetItem(time));
+            ui->Expense_Table->setItem(rowCount, 2, new QTableWidgetItem(name));
+            ui->Expense_Table->setItem(rowCount, 3, new QTableWidgetItem(QString::number(quantity)));
+            ui->Expense_Table->setItem(rowCount, 4, priceItem2);
         }
 
         ui->Statement_Table->setItem(row, 4, priceItem);
